dd16c5583d18e29800b02bc6fafa014527f95224
ameetmadan <ameetmadan@gmail.com>
2025-06-10 10:45:22 +0200

    init

---
+++ calculateOrderTotal.js
@@ -0,0 +1,9 @@
+// JavaScript - What could go wrong?
+function calculateOrderTotal(items, taxRate, discountCode) {
+    console.log("working")
+    // Is items an array? What's in it?
+    // Is taxRate a number or percentage?
+    // What if discountCode is undefined?
+}
+
+calculateOrderTotal(1, 2, 3)
